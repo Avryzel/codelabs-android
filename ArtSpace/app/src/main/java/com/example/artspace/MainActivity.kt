6152aeda7fb1ebed4510574bc3e3fd50b7ac6163
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-06-03 12:31:45 +0700

    fixing some layout

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,14 +16,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
@@ -45,14 +49,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceLayout() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        ArtworkWall()
-        ArtworkDescriptor()
-        Spacer(modifier = Modifier.height(32.dp))
-        DisplayController()
+        Column(
+            modifier = Modifier
+                .widthIn(max = 400.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ArtworkWall()
+            Spacer(modifier = Modifier.height(16.dp))
+            ArtworkDescriptor()
+            Spacer(modifier = Modifier.height(26.dp))
+            DisplayController()
+        }
     }
 }
 
@@ -60,21 +75,33 @@ fun ArtSpaceLayout() {
 fun ArtworkWall() {
     val image = painterResource(R.drawable.mogoon_art01)
 
-    Image(
-        painter = image,
-        contentDescription = "モ誰 Art",
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(40.dp)
-    )
+            .background(Color.LightGray)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(32.dp)
+        )
+    }
 }
 
 @Composable
 fun ArtworkDescriptor() {
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(Color.LightGray),
+
+        horizontalAlignment = Alignment.Start
+    ) {
         Text(
-            text = stringResource(R.string.artwork_title01)
-        )
+            text = stringResource(R.string.artwork_title01),
+
+            )
         Text(
             text = stringResource(R.string.artwork_artist01)
         )
@@ -83,15 +110,20 @@ fun ArtworkDescriptor() {
 
 @Composable
 fun DisplayController() {
-    Row {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
         Button(
-            onClick = {}
+            onClick = {},
+            modifier = Modifier.width(130.dp)
         ) {
             Text(stringResource(R.string.previous_button))
         }
-        Spacer(modifier = Modifier.width(20.dp))
         Button(
-            onClick = {}
+            onClick = {},
+            modifier = Modifier.width(130.dp)
         ) {
             Text(stringResource(R.string.next_button))
         }
