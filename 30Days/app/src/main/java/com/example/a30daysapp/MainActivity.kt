b7f70144d699f1def2f47f35ceecb6bcb124b5b9
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-08-12 21:39:02 +0700

    feat: implement TaskItem with label, image, and info

--- 30Days/app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ 30Days/app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,7 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30daysapp.model.Task
+import com.example.a30daysapp.model.TasksDataSource
 import com.example.a30daysapp.ui.theme._30DaysAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +52,9 @@ fun _30DaysApp() {
         topBar = { _30DaysTopBar() }
     ) { it ->
         LazyColumn(contentPadding = it) {
-
+            items(TasksDataSource.tasks) { task ->
+                TaskItem(tasks = task)
+            }
         }
     }
 }
@@ -57,6 +70,60 @@ fun _30DaysTopBar() {
             )
         }
     )
+}
+
+@Composable
+fun TaskItem(
+    tasks: Task,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+        ) {
+            TaskLabel(taskDay = tasks.dayRes)
+            TaskImage(taskImage = tasks.imageRes)
+            TaskInfo(taskTitle = tasks.titleRes, taskDesc = tasks.descriptionRes)
+        }
+    }
+}
+
+@Composable
+fun TaskLabel(
+    @StringRes taskDay: Int,
+) {
+    Text(
+        text = stringResource(taskDay)
+    )
+}
+
+@Composable
+fun TaskImage(
+    @DrawableRes taskImage: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(taskImage),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun TaskInfo(
+    @StringRes taskTitle: Int,
+    @StringRes taskDesc: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(taskTitle)
+        )
+        Text(
+            text = stringResource(taskDesc)
+        )
+    }
 }
 
 @SuppressLint("ComposableNaming")
