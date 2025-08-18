0f4313d3247a5f8f63e22361ade8c33b0c80d0ce
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-08-18 19:40:06 +0700

    feat: add 28 more data tasks

--- 30Days/app/src/main/java/com/example/a30daysapp/model/TasksDataSource.kt
+++ 30Days/app/src/main/java/com/example/a30daysapp/model/TasksDataSource.kt
@@ -15,6 +15,174 @@ object TasksDataSource {
             imageRes = R.drawable.ic_day1_book_kanji,
             titleRes = R.string.day_2_title,
             descriptionRes = R.string.day_2_descriptions
-        )
+        ),
+        Task(
+            dayRes = R.string.day_3_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_3_title,
+            descriptionRes = R.string.day_3_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_4_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_4_title,
+            descriptionRes = R.string.day_4_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_5_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_5_title,
+            descriptionRes = R.string.day_5_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_6_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_6_title,
+            descriptionRes = R.string.day_6_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_7_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_7_title,
+            descriptionRes = R.string.day_7_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_8_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_8_title,
+            descriptionRes = R.string.day_8_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_9_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_9_title,
+            descriptionRes = R.string.day_9_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_10_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_10_title,
+            descriptionRes = R.string.day_10_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_11_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_11_title,
+            descriptionRes = R.string.day_11_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_12_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_12_title,
+            descriptionRes = R.string.day_12_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_13_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_13_title,
+            descriptionRes = R.string.day_13_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_14_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_14_title,
+            descriptionRes = R.string.day_14_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_15_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_15_title,
+            descriptionRes = R.string.day_15_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_16_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_16_title,
+            descriptionRes = R.string.day_16_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_17_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_17_title,
+            descriptionRes = R.string.day_17_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_18_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_18_title,
+            descriptionRes = R.string.day_18_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_19_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_19_title,
+            descriptionRes = R.string.day_19_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_20_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_20_title,
+            descriptionRes = R.string.day_20_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_21_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_21_title,
+            descriptionRes = R.string.day_21_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_22_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_22_title,
+            descriptionRes = R.string.day_22_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_23_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_23_title,
+            descriptionRes = R.string.day_23_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_24_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_24_title,
+            descriptionRes = R.string.day_24_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_25_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_25_title,
+            descriptionRes = R.string.day_25_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_26_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_26_title,
+            descriptionRes = R.string.day_26_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_27_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_27_title,
+            descriptionRes = R.string.day_27_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_28_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_28_title,
+            descriptionRes = R.string.day_29_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_29_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_29_title,
+            descriptionRes = R.string.day_29_descriptions
+        ),
+        Task(
+            dayRes = R.string.day_30_number,
+            imageRes = R.drawable.ic_day1_book_kanji,
+            titleRes = R.string.day_30_title,
+            descriptionRes = R.string.day_30_descriptions
+        ),
     )
 }
