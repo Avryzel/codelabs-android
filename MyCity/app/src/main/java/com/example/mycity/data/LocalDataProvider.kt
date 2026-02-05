19edf7928d1202346f44bd90580108d0ae62333d
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-02-05 20:45:57 +0700

    feat: expand places data to 4 per category

--- MyCity/app/src/main/java/com/example/mycity/data/LocalDataProvider.kt
+++ MyCity/app/src/main/java/com/example/mycity/data/LocalDataProvider.kt
@@ -1,6 +1,5 @@
 package com.example.mycity.data
 
-import androidx.compose.ui.res.stringResource
 import com.example.mycity.R
 
 object LocalDataProvider {
@@ -17,6 +16,16 @@ object LocalDataProvider {
                     nameRes = R.string.historic_site_kota_tua,
                     descRes = R.string.historic_site_kota_tua_desc,
                     imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.historic_site_national_museum,
+                    descRes = R.string.historic_site_national_museum_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.historic_site_istiqlal,
+                    descRes = R.string.historic_site_istiqlal_desc,
+                    imageRes = R.drawable.ic_launcher_background
                 )
             )
         ),
@@ -31,6 +40,16 @@ object LocalDataProvider {
                 Place(
                     nameRes = R.string.street_food_blok_m,
                     descRes = R.string.street_food_blok_m_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.street_food_gultik,
+                    descRes = R.string.street_food_gultik_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.street_food_nasi_goreng_sirih,
+                    descRes = R.string.street_food_nasi_goreng_sirih_desc,
                     imageRes = R.drawable.ic_launcher_background
                 )
             )
@@ -47,6 +66,16 @@ object LocalDataProvider {
                     nameRes = R.string.mall_plaza_indonesia,
                     descRes = R.string.mall_plaza_indonesia_desc,
                     imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.mall_senayan_city,
+                    descRes = R.string.mall_senayan_city_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.mall_kotakasablanka,
+                    descRes = R.string.mall_kotakasablanka_desc,
+                    imageRes = R.drawable.ic_launcher_background
                 )
             )
         ),
@@ -61,6 +90,16 @@ object LocalDataProvider {
                 Place(
                     nameRes = R.string.station_jakarta_kota,
                     descRes = R.string.station_jakarta_kota_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.station_manggarai,
+                    descRes = R.string.station_manggarai_desc,
+                    imageRes = R.drawable.ic_launcher_background
+                ),
+                Place(
+                    nameRes = R.string.station_sudirman,
+                    descRes = R.string.station_sudirman_desc,
                     imageRes = R.drawable.ic_launcher_background
                 )
             )
