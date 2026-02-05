28017399cc9e3ed75a788fe9299ce354def16d04
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-02-05 21:25:33 +0700

    feat: extract MyCityAppBar and implement dynamic navigation logic

--- MyCity/app/src/main/java/com/example/mycity/ui/MyCityApp.kt
+++ MyCity/app/src/main/java/com/example/mycity/ui/MyCityApp.kt
@@ -3,8 +3,12 @@ package com.example.mycity.ui
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +20,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.mycity.R
 import com.example.mycity.data.LocalDataProvider
@@ -28,14 +33,49 @@ enum class MyAppScreen(@StringRes val title: Int) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun MyCityAppBar(
+    currentScreen: MyAppScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        modifier = modifier,
+
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun MyCityApp(
     viewModel: MyCityViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = MyAppScreen.valueOf(
+        backStackEntry?.destination?.route ?: MyAppScreen.Category.name
+    )
+
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(stringResource(R.string.app_name)) },
+            MyCityAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+
+                navigateUp = { navController.navigateUp() }
             )
         }
     ) { innerPadding ->
